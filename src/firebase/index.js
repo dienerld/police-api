0193eec37d1b4998a96fd557dc875f2aac86848d
Diener Dornelas <dienezim@hotmail.com>
2022-12-17 15:28:17 -0300

    update dependencies

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,8 +1,8 @@
-import "dotenv/config";
+import 'dotenv/config';
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,10 +15,10 @@ const firebaseConfig = {
 	storageBucket: process.env.STORAGE_BUCKET,
 	messagingSenderId: process.env.MESSAGING_SENDER_ID,
 	appId: process.env.APP_ID,
-	measurementId: process.env.MEASUREMENT_ID,
+	measurementId: process.env.MEASUREMENT_ID
 };
 
-export function firebase() {
+export function firebase () {
 	// Initialize Firebase
 	const app = initializeApp(firebaseConfig);
 	const analytics = getAnalytics(app);
